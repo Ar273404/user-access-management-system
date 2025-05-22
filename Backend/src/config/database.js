d1f07083efd429e5c69cfd1f880e5c2955b84851
Ar273404 <arun273404@gmail.com>
2025-05-22 09:56:42 +0530

    push

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,6 +1,29 @@
+// // const { DataSource } = require("typeorm");
+// // const dotenv = require("dotenv");
+
+// // dotenv.config();
+
+// // const { User } = require("../entities/User.js");
+// // const { Software } = require("../entities/Software.js");
+// // const { Request } = require("../entities/Request.js");
+
+// // const AppDataSource = new DataSource({
+// //   type: "postgres",
+// //   host: process.env.DB_HOST,
+// //   port: parseInt(process.env.DB_PORT, 10),
+// //   username: process.env.DB_USERNAME,
+// //   password: process.env.DB_PASSWORD,
+// //   database: process.env.DB_NAME,
+// //   synchronize: true,
+// //   logging: true,
+// //   entities: [User, Software, Request],
+// // });
+// // // console.log(AppDataSource)
+
+// // module.exports = AppDataSource;
+
 // const { DataSource } = require("typeorm");
 // const dotenv = require("dotenv");
-
 // dotenv.config();
 
 // const { User } = require("../entities/User.js");
@@ -10,17 +33,17 @@
 // const AppDataSource = new DataSource({
 //   type: "postgres",
 //   host: process.env.DB_HOST,
-//   port: parseInt(process.env.DB_PORT, 10),
+//   port: parseInt(process.env.DB_PORT),
 //   username: process.env.DB_USERNAME,
 //   password: process.env.DB_PASSWORD,
 //   database: process.env.DB_NAME,
 //   synchronize: true,
 //   logging: true,
-//   entities: [User, Software, Request],
+//   entities: [User, Software, Request], // ✅ these must be EntitySchemas
 // });
-// // console.log(AppDataSource)
 
 // module.exports = AppDataSource;
+
 
 const { DataSource } = require("typeorm");
 const dotenv = require("dotenv");
@@ -32,14 +55,13 @@ const { Request } = require("../entities/Request.js");
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   synchronize: true,
   logging: true,
-  entities: [User, Software, Request], // ✅ these must be EntitySchemas
+  ssl: {
+    rejectUnauthorized: false, // Required by Render for secure connection
+  },
+  entities: [User, Software, Request],
 });
 
 module.exports = AppDataSource;
